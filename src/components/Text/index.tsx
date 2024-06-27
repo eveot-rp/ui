134ed69ef6cd90889171dfd805599c5a0197728a
BADtochka <badmodertwitch@gmail.com>
2024-06-27 15:59:29 +0300

    feat: update Text types

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,19 +1,30 @@
-import '@assets/styles/app.scss'
-import { FC, PropsWithChildren } from 'react'
-import { TypographyColors, TypographySizes } from './constants'
+import '@assets/styles/app.scss';
+import { FC, HTMLAttributes, PropsWithChildren } from 'react';
+import { TypographyColors, TypographySizes } from './constants';
 
-type TypographySizes = keyof typeof TypographySizes
-type TypographyColors = keyof typeof TypographyColors
-type TypographyWeights = '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
+type TypographySizes = keyof typeof TypographySizes;
+type TypographyColors = keyof typeof TypographyColors;
+type TypographyWeights = '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
 
-type TextProps = {
-  size: TypographySizes,
-  color?: TypographyColors
-  weight?: TypographyWeights
+interface TextProps extends HTMLAttributes<HTMLSpanElement> {
+  size: TypographySizes;
+  color?: TypographyColors;
+  weight?: TypographyWeights;
 }
 
-export const Text: FC<PropsWithChildren<TextProps>> = ({ children, size = 'h1', color = 'b000', weight = '500' }) => {
+export const Text: FC<PropsWithChildren<TextProps>> = ({
+  children,
+  size = 'h1',
+  color = 'b000',
+  weight = '500',
+  ...props
+}) => {
   return (
-    <span style={{ fontSize: TypographySizes[size], color: TypographyColors[color], fontWeight: weight }}>{ children }</span>
-  )
-}
+    <span
+      style={{ fontSize: TypographySizes[size], color: TypographyColors[color], fontWeight: weight }}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+};
