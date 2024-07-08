813433f89e3cc4a08ece233e5cd2271b58b87f48
BADtochka <badmodertwitch@gmail.com>
2024-07-08 15:06:23 +0300

    feat: update ButtonProps types

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,15 @@
-import { Icon, IconName } from '@components/Icon'
-import { FC, MouseEvent, PropsWithChildren } from 'react'
-import './style.scss'
+import { Icon, IconName } from '@components/Icon';
+import { ButtonHTMLAttributes, FC, PropsWithChildren } from 'react';
+import './style.scss';
 
 // "prop?" - because Partial<> broke the doc page
-export interface ButtonProps {
-  name?: string
-  style?: 'dark' | 'light'
-  size?: 'xs' | 'sm' | 'md'
-  iconLeft?: IconName
-  iconRight?: IconName
-  disabled?: boolean
-  active?: boolean
-  centered?: boolean
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void
+export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'style'> {
+  style?: 'dark' | 'light';
+  size?: 'xs' | 'sm' | 'md';
+  iconLeft?: IconName;
+  iconRight?: IconName;
+  active?: boolean;
+  centered?: boolean;
 }
 
 export const Button: FC<PropsWithChildren<ButtonProps>> = ({
@@ -27,20 +24,20 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = ({
   active,
   ...props
 }) => {
-  return (  
+  return (
     <button
-      name={ name }
-      className="ev-button"
-      data-style={ style }
-      data-size={ size }
-      disabled={ disabled }
-      data-active={ active }
-      data-centered={ centered }
+      name={name}
+      className='ev-button'
+      data-style={style}
+      data-size={size}
+      disabled={disabled}
+      data-active={active}
+      data-centered={centered}
       {...props}
     >
-      { iconLeft && <Icon name={iconLeft} /> }
+      {iconLeft && <Icon name={iconLeft} />}
       {children}
-      { iconRight && <Icon name={iconRight} /> }
+      {iconRight && <Icon name={iconRight} />}
     </button>
-  )
-}
+  );
+};
