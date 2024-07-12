41a4a11bbca240cb7c7b11b390f015121c997743
BADtochka <BADtochka@users.noreply.github.com>
2024-07-12 07:38:31 +0000

    deploy: 89e388c8a4d6870a97e61693b8727cba8baf051f

--- assets/Text.stories-vC9To_U6.js
+++
@@ -1,5 +0,0 @@
-import{T as o}from"./index-DGrJxiR5.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const m={component:o,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{children:"Text"}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: 'Text'
-  }
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,m as default};
