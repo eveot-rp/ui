ac4db7801159f3fee505507e16927e58fd4760e7
BADtochka <BADtochka@users.noreply.github.com>
2024-07-22 16:57:48 +0000

    deploy: 1542666808ba27c4f5f45efa12b6c08b39103b1d

--- components/ColorSelector/index.d.ts
+++ components/ColorSelector/index.d.ts
@@ -1,8 +1,9 @@
 import { FC, HTMLAttributes } from '../../../node_modules/react';
 
-export interface ColorSelectorProps extends Omit<HTMLAttributes<HTMLDivElement>, "onChange"> {
+export interface ColorSelectorProps extends Omit<HTMLAttributes<HTMLDivElement>, 'onChange'> {
     perSwitch?: number;
     disabled?: boolean;
+    currentIndex: number;
     onChange?: (value: number) => void;
 }
 export declare const ColorSelector: FC<ColorSelectorProps>;
