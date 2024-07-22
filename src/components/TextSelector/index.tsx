491f01843e9405da5eb89d2493dfa38a1890b089
BADtochka <badmodertwitch@gmail.com>
2024-07-22 11:51:25 +0300

    feat: update TextSelector props

--- src/components/TextSelector/index.tsx
+++ src/components/TextSelector/index.tsx
@@ -1,26 +1,23 @@
-import { Icon } from "@components/Icon";
-import { FC, HTMLAttributes, useEffect, useState } from "react";
-import "./style.scss";
+import { Icon } from '@components/Icon';
+import { CSSProperties, FC, HTMLAttributes, useEffect, useState } from 'react';
+import './style.scss';
 
-export interface TextSelectorProps
-  extends Omit<HTMLAttributes<HTMLDivElement>, "onChange"> {
-  items?: Array<string>;
+export interface TextSelectorProps extends Omit<HTMLAttributes<HTMLDivElement>, 'onChange'> {
+  items?: Array<{
+    id: string;
+    label: string;
+  }>;
+  width: CSSProperties['width'];
   disabled?: boolean;
-  onChange?: (text: string, index: number) => void;
+  onChange?: (id: string) => void;
 }
 
-export const TextSelector: FC<TextSelectorProps> = ({
-  items,
-  disabled,
-  onChange,
-  className,
-  ...props
-}) => {
+export const TextSelector: FC<TextSelectorProps> = ({ items, disabled, onChange, className, width, ...props }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const onChangeItem = (variant: "prev" | "next") => {
+  const onChangeItem = (variant: 'prev' | 'next') => {
     if (!items) return;
-    if (variant === "prev") {
+    if (variant === 'prev') {
       setSelectedIndex((selectedIndex + items.length - 1) % items.length);
     } else {
       setSelectedIndex((selectedIndex + 1) % items.length);
@@ -29,30 +26,33 @@ export const TextSelector: FC<TextSelectorProps> = ({
 
   useEffect(() => {
     if (onChange) {
-      const text = items ? items[selectedIndex] : "";
-      onChange(text, selectedIndex);
+      const text = items ? items[selectedIndex].id : '';
+      onChange(text);
     }
   }, [selectedIndex]);
+
+  const findedItem = items?.find((_, index) => index === selectedIndex);
+  if (!findedItem) return null;
 
   return (
     <div
       className={`ev-text-selector ${className}`}
       data-disabled={disabled}
+      style={{ width }}
       {...props}
     >
       <Icon
-        name="TbSquareArrowLeftFilled"
-        size="1.75rem"
-        onClick={() => onChangeItem("prev")}
+        name='TbSquareArrowLeftFilled'
+        size='1.75rem'
+        onClick={() => onChangeItem('prev')}
       />
-      <div className="ev-text-selector-value">
-        {items?.find((_, index) => index === selectedIndex)}
-      </div>
+      <div className='ev-text-selector-value'>{findedItem.label}</div>
       <Icon
-        name="TbSquareArrowRightFilled"
-        size="1.75rem"
-        onClick={() => onChangeItem("next")}
+        name='TbSquareArrowRightFilled'
+        size='1.75rem'
+        onClick={() => onChangeItem('next')}
       />
     </div>
   );
 };
+
--- src/stories/TextSelector.stories.ts
+++ src/stories/TextSelector.stories.ts
@@ -6,11 +6,29 @@ export default {
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    width: {
+      type: 'string',
+    },
+  },
   tags: ['autodocs'],
 } as Meta<typeof TextSelector>;
 
 export const Default: StoryObj<typeof TextSelector> = {
   args: {
-    items: ['Text 1', 'Text 2', 'Text 3', 'Text 4'],
-  }
+    items: [
+      {
+        id: '1',
+        label: 'Text 1',
+      },
+      {
+        id: '2',
+        label: 'Text 2',
+      },
+      {
+        id: '3',
+        label: 'Text 3',
+      },
+    ],
+  },
 };
