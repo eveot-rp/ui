49ee2ceeae04763af8215bfe96f36d69fd80cf55
BADtochka <BADtochka@users.noreply.github.com>
2024-07-23 12:27:20 +0000

    deploy: 9bbdfc3e3dc10f3456d3ca93f4af456dda6d3f9d

--- components/TextSelector/index.d.ts
+++ components/TextSelector/index.d.ts
@@ -1,13 +1,15 @@
 import { CSSProperties, FC, HTMLAttributes } from '../../../node_modules/react';
 
+type SelectorItem = {
+    id: number;
+    label: string;
+};
 export interface TextSelectorProps extends Omit<HTMLAttributes<HTMLDivElement>, 'onChange'> {
-    items: Array<{
-        id: number;
-        label: string;
-    }>;
+    items: SelectorItem[];
     width?: CSSProperties['width'];
     disabled?: boolean;
     currentId?: number;
     onChange?: (id: number) => void;
 }
 export declare const TextSelector: FC<TextSelectorProps>;
+export {};
