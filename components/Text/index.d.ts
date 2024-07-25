8c031ae3a1564c0e315ccccac42c74d788ab9c72
BADtochka <BADtochka@users.noreply.github.com>
2024-07-25 13:05:30 +0000

    deploy: 6f1e8647ee982a615b4df0e6c596a8564650b69e

--- components/Text/index.d.ts
+++ components/Text/index.d.ts
@@ -3,8 +3,15 @@ import { TypographyColors, TypographySizes } from './constants';
 
 type TypographySizes = keyof typeof TypographySizes;
 type TypographyColors = keyof typeof TypographyColors;
-type TypographyWeights = "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
+type TypographyWeights = '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
 export interface TextProps extends HTMLAttributes<HTMLSpanElement> {
+    /**
+     * - h1: 1.5rem (24px)
+     * - h2: 1.25rem (20px)
+     * - h3: 1rem (16px)
+     * - subheading: 0.875rem (14px)
+     * - body2: 0.625rem (12px)
+     */
     size?: TypographySizes;
     color?: TypographyColors;
     weight?: TypographyWeights;
