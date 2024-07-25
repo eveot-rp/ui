a3e06d9067bf159474fd5a9f31e1b79f0674e3aa
BADtochka <BADtochka@users.noreply.github.com>
2024-07-25 17:51:16 +0000

    deploy: 60bf76641fea50c81686c4b5e7067eb1984b4465

--- components/CircularProgress/index.d.ts
+++ components/CircularProgress/index.d.ts
@@ -4,6 +4,7 @@ export interface CircularProgressProps extends HTMLAttributes<HTMLDivElement> {
     progress: number;
     max?: number;
     width?: string;
+    radius?: number;
     strokeWidth?: string;
 }
 export declare const CircularProgress: FC<PropsWithChildren<CircularProgressProps>>;
