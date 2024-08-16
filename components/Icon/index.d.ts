751f62612233b62401748ab74ad6d01a9255a1d5
Gleef132 <Gleef132@users.noreply.github.com>
2024-08-16 11:37:21 +0000

    deploy: 9267529995cb551469d84429e218ba71de0f8de3

--- components/Icon/index.d.ts
+++ components/Icon/index.d.ts
@@ -1,15 +1,15 @@
 import { FC, HTMLAttributes } from '../../../node_modules/react';
 import { IconBaseProps } from 'react-icons';
 
-import * as tbicons from 'react-icons/tb';
-import * as evicons from './icons';
+import * as tbicons from "react-icons/tb";
+import * as evicons from "./icons";
 type TablerIconName = keyof typeof tbicons;
 type EveotIconName = keyof typeof evicons;
 export type IconName = TablerIconName | EveotIconName;
 export interface IconProps extends HTMLAttributes<HTMLDivElement> {
     name: IconName;
-    size?: IconBaseProps['size'];
-    color?: IconBaseProps['color'];
+    size?: IconBaseProps["size"];
+    color?: IconBaseProps["color"];
     onClick?: () => void;
 }
 export declare const IconNames: string[];
