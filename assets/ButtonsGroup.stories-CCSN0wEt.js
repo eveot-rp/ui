30113dcafe5f8650cfaff7854be8946e6fa36918
Gleef132 <Gleef132@users.noreply.github.com>
2024-08-16 12:49:31 +0000

    deploy: 0d0761c70a53176d9b37f6e8c38ed1a782d68586

---
+++ assets/ButtonsGroup.stories-CCSN0wEt.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-DEdD30eg.js";import{B as e}from"./index-DmgOCwy4.js";import"./index-RYns6xqu.js";import"./index-b5Xo1QQn.js";const s=({children:o,className:a,...i})=>t.jsx("div",{className:`ev-buttons-group ${a??""}`,...i,children:o});try{s.displayName="ButtonsGroup",s.__docgenInfo={description:"",displayName:"ButtonsGroup",props:{}}}catch{}const m={component:s,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{},render:o=>t.jsxs(s,{...o,children:[t.jsx(e,{style:"light",children:"Button 1"}),t.jsx(e,{style:"light",children:"Button 2"}),t.jsx(e,{style:"light",children:"Button 2"})]})};var n,u,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  render: props => <ButtonsGroup {...props}>
+      <Button style="light">Button 1</Button>
+      <Button style="light">Button 2</Button>
+      <Button style="light">Button 2</Button>
+    </ButtonsGroup>
+}`,...(p=(u=r.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};const g=["Preview"];export{r as Preview,g as __namedExportsOrder,m as default};
