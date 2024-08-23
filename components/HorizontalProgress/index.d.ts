645705c69a318331f0e37299792d2e6fa7703bcd
BADtochka <BADtochka@users.noreply.github.com>
2024-08-23 07:54:15 +0000

    deploy: 112aae35ab2d0f00e857e388fbce7d79385cc400

--- components/HorizontalProgress/index.d.ts
+++ components/HorizontalProgress/index.d.ts
@@ -2,7 +2,8 @@ import { FC, HTMLAttributes } from '../../../node_modules/react';
 
 export interface HorizontalProgressProps extends HTMLAttributes<HTMLDivElement> {
     progress: number;
+    max?: number;
     disabled?: boolean;
-    size: 'sm' | 'md';
+    size?: 'sm' | 'md';
 }
 export declare const HorizontalProgress: FC<HorizontalProgressProps>;
