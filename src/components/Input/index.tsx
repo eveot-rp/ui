c348f386d576e9eeec0943ce08dfc3a4965c15a5
Janybek Asanakunov <zhanybek.asanakunov@bk.ru>
2024-09-11 16:13:58 +0600

    add white space for text info in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,27 +1,41 @@
-import { Icon, IconName } from '@components/Icon';
-import { TextInfo, TextInfoProps } from '@components/TextInfo';
-import { combineRefs } from '@utils/combineRefs';
-import { forwardRef, InputHTMLAttributes, MouseEvent, PropsWithChildren, useCallback, useRef } from 'react';
-import './style.scss';
+import { Icon, IconName } from "@components/Icon";
+import { TextInfo, TextInfoProps } from "@components/TextInfo";
+import { combineRefs } from "@utils/combineRefs";
+import {
+  forwardRef,
+  InputHTMLAttributes,
+  MouseEvent,
+  PropsWithChildren,
+  useCallback,
+  useRef,
+} from "react";
+import "./style.scss";
 
-export interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'style' | 'type'> {
-  type?: 'text' | 'number' | 'password';
+export interface InputProps
+  extends Omit<
+    InputHTMLAttributes<HTMLInputElement>,
+    "size" | "style" | "type"
+  > {
+  type?: "text" | "number" | "password";
   label?: string;
   description?: string;
-  size?: 'xs' | 'sm' | 'md';
-  style?: 'light' | 'dark';
+  size?: "xs" | "sm" | "md";
+  style?: "light" | "dark";
   invalid?: boolean;
   iconLeft?: IconName;
   iconRight?: IconName;
   textInfo?: TextInfoProps;
 }
 
-export const Input = forwardRef<HTMLInputElement, PropsWithChildren<InputProps>>(
+export const Input = forwardRef<
+  HTMLInputElement,
+  PropsWithChildren<InputProps>
+>(
   (
     {
-      style = 'dark',
-      size = 'md',
-      type = 'text',
+      style = "dark",
+      size = "md",
+      type = "text",
       invalid,
       label,
       description,
@@ -53,31 +67,31 @@ export const Input = forwardRef<HTMLInputElement, PropsWithChildren<InputProps>>
 
     return (
       <div
-        className={`ev-input ${className ?? ''}`}
+        className={`ev-input ${className ?? ""}`}
         data-style={style}
         data-invalid={invalid}
         data-size={size}
       >
         {label && <label>{label}</label>}
-        <div
-          className='ev-input-wrapper'
-          onClick={onComponentClick}
-        >
+        <div className="ev-input-wrapper" onClick={onComponentClick}>
           {iconLeft && <Icon name={iconLeft} />}
           <input
             ref={combineRefs([parentRef, inputRef])}
             onChange={onChange}
             onClick={onInputClick}
-            type={type !== 'number' ? type : 'text'}
+            type={type !== "number" ? type : "text"}
             {...props}
           />
           {iconRight && <Icon name={iconRight} />}
-          {textInfo && <TextInfo {...textInfo} />}
+          {textInfo && (
+            <TextInfo {...textInfo} style={{ whiteSpace: "nowrap" }} />
+          )}
           {children}
         </div>
-        {description && <span className='ev-input-description'>{description}</span>}
+        {description && (
+          <span className="ev-input-description">{description}</span>
+        )}
       </div>
     );
   }
 );
-
