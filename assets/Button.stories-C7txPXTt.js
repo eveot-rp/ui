67c4f0d3283bc7fa6c870c4a86cc768724069b4c
BADtochka <BADtochka@users.noreply.github.com>
2024-09-12 09:47:29 +0000

    deploy: 059ee2d33692998021e2b631abbd173e762d0765

--- assets/Button.stories-C7txPXTt.js
+++
@@ -1,9 +0,0 @@
-import{B as r}from"./index-BZyAlf9m.js";import{I as t}from"./index-M8lrpcL7.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./index-rDsjxskc.js";import"./colors-NuYFRpnV.js";const l={component:r,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{iconLeft:{options:t},iconRight:{options:t}}},e={args:{children:"Button",size:"md",style:"dark",disabled:!1,active:!1}};var o,s,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: 'Button',
-    size: 'md',
-    style: 'dark',
-    disabled: false,
-    active: false
-  }
-}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,l as default};
