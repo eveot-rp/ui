cb5b1b849324b58c97dfe27c15118fd1a8cdc363
dungeonmaz <dungeonmaz@users.noreply.github.com>
2024-11-07 13:12:41 +0000

    deploy: 4201d42f4761cb21515841e2052c96a95d4e998b

--- assets/Icon.stories-CqLVUfVD.js
+++
@@ -1,7 +0,0 @@
-import{a as s,I as t}from"./index-CJ5-AOdJ.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./index-DzF68b7t.js";import"./colors-NuYFRpnV.js";const d={component:s,parameters:{layout:"centered"},argTypes:{name:{options:t}},tags:["autodocs"]},e={args:{name:"TbAlarm",size:"10rem",color:"white"}};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    name: 'TbAlarm',
-    size: '10rem',
-    color: 'white'
-  }
-}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const l=["Preview"];export{e as Preview,l as __namedExportsOrder,d as default};
