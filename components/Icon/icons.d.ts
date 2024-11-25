fd972d6f4266ca0e5a0f846bbda0ac0734da00d7
dungeonmaz <dungeonmaz@users.noreply.github.com>
2024-11-25 14:54:31 +0000

    deploy: 6f693c634780a29b69fac84b1abe4e56d69e01b7

--- components/Icon/icons.d.ts
+++ components/Icon/icons.d.ts
@@ -7,6 +7,9 @@ import { SVGProps } from '../../../node_modules/react';
  * @param {SVGSVGElement} stroke currentColor
  */
 type EvIconProps = SVGProps<SVGSVGElement>;
+export declare const EvTimer: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvStopwatch: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvAlarm: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvNav: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvCard: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvVotes: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
