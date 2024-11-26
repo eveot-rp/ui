317542b8b4a9fc93c2993645aca6e8ab70f2cdb5
dungeonmaz <dungeonmaz@users.noreply.github.com>
2024-11-26 09:03:22 +0000

    deploy: 889e6cd2cc74a5e97c37339b02295e541838f6ee

--- assets/TextInfo.stories-C8Psa9sB.js
+++
@@ -1,5 +0,0 @@
-import{I as r}from"./index-ByKzCu9o.js";import{T as s}from"./index-b6H7gZhT.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./index-CWzXfiof.js";import"./colors-NuYFRpnV.js";const d={component:s,parameters:{layout:"centered"},argTypes:{iconLeft:{options:r},iconRight:{options:r},bg:{type:"string",control:"color"},borderColor:{type:"string",control:"color"}},tags:["autodocs"]},o={args:{children:"Content"}};var t,e,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: 'Content'
-  }
-}`,...(n=(e=o.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,d as default};
--- components/Icon/icons.d.ts
+++ components/Icon/icons.d.ts
@@ -7,6 +7,7 @@ import { SVGProps } from '../../../node_modules/react';
  * @param {SVGSVGElement} stroke currentColor
  */
 type EvIconProps = SVGProps<SVGSVGElement>;
+export declare const EVPaint: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EVExtra: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EVService: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EVTuning: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
