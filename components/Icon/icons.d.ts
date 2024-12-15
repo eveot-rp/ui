0dad7691eb885d82e9dd78d2d3b4dbdc5930fdd5
Gleef132 <Gleef132@users.noreply.github.com>
2024-12-15 13:29:02 +0000

    deploy: 6effdce48cb8740a5b4cd4a8a326f6a13d6669c7

--- components/Icon/icons.d.ts
+++ components/Icon/icons.d.ts
@@ -7,6 +7,8 @@ import { SVGProps } from '../../../node_modules/react';
  * @param {SVGSVGElement} stroke currentColor
  */
 type EvIconProps = SVGProps<SVGSVGElement>;
+export declare const EVTickSquare: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EVTimer: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvPaint2: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvBrakes2: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvSuspension2: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
