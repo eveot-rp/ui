554ae4e7a187f4c71cc7dcdc415f1b96d0beb490
Gleef132 <brawlsrarts@gmail.com>
2024-12-27 02:15:33 +0600

    feat: add icons

--- src/stories/Icon.stories.ts
+++ src/stories/Icon.stories.ts
@@ -1,23 +1,23 @@
-import { Icon, IconNames } from '@components/Icon';
-import { Meta, StoryObj } from '@storybook/react';
+import { Icon, IconNames } from "@components/Icon";
+import { Meta, StoryObj } from "@storybook/react";
 
 export default {
   component: Icon,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
   argTypes: {
     name: {
-      options: IconNames
-    }
+      options: IconNames,
+    },
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
 } as Meta<typeof Icon>;
 
 export const Preview: StoryObj<typeof Icon> = {
   args: {
-    name: 'TbAlarm',
-    size: '10rem',
-    color: 'white'
-  }
+    name: "TbAlarm",
+    size: "10rem",
+    color: "white",
+  },
 };
