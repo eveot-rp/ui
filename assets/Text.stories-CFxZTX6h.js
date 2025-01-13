ec8c764d50796001e4befa927c25da393ab531e0
dungeonmaz <dungeonmaz@users.noreply.github.com>
2025-01-13 05:26:20 +0000

    deploy: cfd20157cbb0ba296bab2f807ede65c0cf50255e

--- assets/Text.stories-CFxZTX6h.js
+++
@@ -1,5 +0,0 @@
-import{T as s}from"./index-DaOkerVy.js";import"./jsx-runtime-CmIOflP4.js";import"./index-KqYmeiyw.js";import"./colors-NuYFRpnV.js";const m={component:s,parameters:{layout:"centered"},argTypes:{color:{type:"string",control:"color"}},tags:["autodocs"]},r={args:{children:"Text"}};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    children: 'Text'
-  }
-}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Default"];export{r as Default,d as __namedExportsOrder,m as default};
