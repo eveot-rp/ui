0ac11650a546060d6038b7c69091a556d88a3069
dungeonmaz <dungeonmaz@users.noreply.github.com>
2025-01-26 11:11:56 +0000

    deploy: 2f46a1049ef7dac2ba6b93e6803725c5e204141c

--- components/Icon/icons.d.ts
+++ components/Icon/icons.d.ts
@@ -7,6 +7,13 @@ import { SVGProps } from '../../../node_modules/react';
  * @param {SVGSVGElement} stroke currentColor
  */
 type EvIconProps = SVGProps<SVGSVGElement>;
+export declare const EvEveot: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvVolumeWave3: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvVolumeWave1: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvBackward: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvForward: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvStopCircle: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const EvPlayCircle: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvThirst: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvHunger: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvGasStation: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
