e9f1b77aa898235990e20c8ad6e73567ee7e1c7a
Gleef132 <Gleef132@users.noreply.github.com>
2025-02-21 16:37:49 +0000

    deploy: 5a4081b45c0552001d7b934ad6cbff1a78974987

--- components/Icon/icons.d.ts
+++ components/Icon/icons.d.ts
@@ -7,6 +7,7 @@ import { SVGProps } from '../../../node_modules/react';
  * @param {SVGSVGElement} stroke currentColor
  */
 type EvIconProps = SVGProps<SVGSVGElement>;
+export declare const EvLock2: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvChevronUpSquare: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvChevronDownSquare: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
 export declare const EvKey: (props: EvIconProps) => import("react/jsx-runtime").JSX.Element;
